5576949003f3b3b2dbbd8a5c8a2d6b2c797651e6
kesavk7b <kesavabharathi2001@gmail.com>
2024-12-30 20:35:47 +0530

    heart in added in text

--- modal.js
+++ modal.js
@@ -1,12 +1,15 @@
 cover = document.getElementById('letter_cover');
 modal = document.getElementById('letter_modal');
+modal_content = document.getElementById('letter_content');
  function openModal(){
     modal.style.display = 'flex';
-    console.log('clicked')
+    modal_content.innerHTML = modal_content.innerHTML.replace(/#/g,'❤️')
+    modal_content.innerHTML = modal_content.innerHTML.replace(/@/g,'💖')
+    
 }
  function closeModal(){
     modal.style.display = 'none';
-    console.log('clicked')
+    
 }
 
 modal.click = () =>{
